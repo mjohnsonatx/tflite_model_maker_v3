3c21075141decb4386b64047fe8d2c48b206d4cd
mjohnASUS <m.johnsonatx@gmail.com>
2024-08-01 11:49:50 -0500

    added some quantization

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ DATA_DIR = 'new data'
 TRAIN_DIR = os.path.join(DATA_DIR, 'train')
 VALID_DIR = os.path.join(DATA_DIR, 'valid')
 TEST_DIR = os.path.join(DATA_DIR, 'test')
-BATCH_SIZE = 4
+BATCH_SIZE = 32
 ARCHITECTURE = 'efficientdet_lite0'
 TRAIN_WHOLE_MODEL = True
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
     model = object_detector.create(
         train,
-        epochs=50,
+        epochs=1,
         model_spec=spec,
         batch_size=BATCH_SIZE,
         train_whole_model=TRAIN_WHOLE_MODEL,
@@ -53,11 +53,20 @@ if __name__ == "__main__":
     if TRAIN_WHOLE_MODEL:
         tflite_filename = f'{ARCHITECTURE}_whole.tflite'
 
-    print("Evaluating the original model...")
-    print(model.evaluate(test, batch_size=BATCH_SIZE))
-
     print("Exporting the model...")
     model.export(export_dir=EXPORT_DIR, tflite_filename=tflite_filename)
 
-    print("Evaluating the exported model...")
-    print(model.evaluate_tflite(os.path.join(EXPORT_DIR, tflite_filename), test))
+    print("Quantizing the model...")
+    quantized_tflite_filename = f'{ARCHITECTURE}_quantized_4_75.tflite'
+    converter = tf.lite.TFLiteConverter.from_keras_model(model.model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    quantized_tflite_model = converter.convert()
+
+    with open(os.path.join(EXPORT_DIR, quantized_tflite_filename), 'wb') as f:
+        f.write(quantized_tflite_model)
+
+    with open(os.path.join(EXPORT_DIR, quantized_tflite_filename), 'wb') as f:
+        f.write(quantized_tflite_model)
+
+    # print("Evaluating the quantized model...")
+    # print(model.evaluate_tflite(os.path.join(EXPORT_DIR, quantized_tflite_filename), test))
